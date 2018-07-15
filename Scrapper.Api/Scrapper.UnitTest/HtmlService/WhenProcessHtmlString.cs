98e25aa8eaa51268f2d0ff4308c647c28e11fc73
shichang <houshichang@hotmail.com>
2018-07-15 23:57:32 +1200

    added unit tests for html processor

---
+++ Scrapper.Api/Scrapper.UnitTest/HtmlService/WhenProcessHtmlString.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Scrapper.Api.HtmlService;
+using Xunit;
+
+namespace Scrapper.UnitTest.HtmlService
+{
+    public class WhenProcessHtmlString
+    {
+        private IHtmlProcessor _htmlProcessor;
+
+        public WhenProcessHtmlString()
+        {
+            _htmlProcessor = new HtmlProcessor();
+        }
+
+        [Fact]
+        public void it_should_return_correct_result()
+        {
+            // setup
+            var givenHtmlString = "<html><body><div><a href=\'www.facebook.com\'/></div><div class=\'g\'><a href=\'www.facebook.com\'/></div><div class=\'g\'><a href=\'www.woolworths.com.au\'/></div><div><a href=\'www.facebook.com\'/></div><div class=\'g\'><a href=\'www.facebook.com\'/></div></body></html>";
+            var givenTarget = "www.facebook.com";
+
+            // act
+            var result = _htmlProcessor.GetSearchPositions(givenHtmlString, givenTarget).ToList();
+
+            // assert
+            Assert.Equal(2, result.Count);
+            Assert.Equal(0, result[0]);
+            Assert.Equal(2, result[1]);
+        }
+    }
+}
